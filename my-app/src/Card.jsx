96caf09a299ef4f71456528c363ce5ba98bfa20f
Elie MANIZABAYO <manizabayoelie.com>
2025-04-12 18:28:21 +0200

    Add user card

--- my-app/src/Card.jsx
+++ my-app/src/Card.jsx
@@ -1,6 +1,12 @@
+import Profile from './assets/Profile.jpg'
 const Card = ()=>{
     return(
-    <h1>My Card</h1>
+     <div className="card">
+        <img alt="profile" src={Profile} ></img>
+        <h2>Elie</h2>
+        <p>Software developer</p>
+
+     </div>
     )
 }
 export default Card
