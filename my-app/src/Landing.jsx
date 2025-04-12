b648346b05026b510b1c88df86b0f988a80fd54e
Elie MANIZABAYO <manizabayoelie.com>
2025-04-12 17:46:35 +0200

    modify landing page

--- my-app/src/Landing.jsx
+++ my-app/src/Landing.jsx
@@ -3,7 +3,15 @@ const Landing = () =>{
     return(
         <div>
             <h1>Welcome To online Restaurant</h1>
-            <h2>OUR MENU</h2>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Services</li>
+                    
+                </ul>
+            </nav>
         </div>
       
 
