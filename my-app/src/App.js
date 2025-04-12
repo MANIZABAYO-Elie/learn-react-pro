235775d26ce0b7d60650735ced90819c52491f58
Elie MANIZABAYO <manizabayoelie.com>
2025-04-12 17:55:24 +0200

    Add user card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Landing from './Landing';
+import Card from './Card';
 
 function App() {
   return (
+    <>
     <Landing/>
+    <Card/>
+    </>
+    
   );
 }
 
---
+++ my-app/src/Card.jsx
@@ -0,0 +1,6 @@
+const Card = ()=>{
+    return(
+    <h1>My Card</h1>
+    )
+}
+export default Card
