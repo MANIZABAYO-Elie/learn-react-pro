fad24df052ee94d2b88a0a2e6a9703ef16d560bf
Elie MANIZABAYO <manizabayoelie.com>
2025-04-12 17:36:37 +0200

    add landing page

---
+++ my-app/src/Landing.jsx
@@ -0,0 +1,13 @@
+const Landing = () =>{
+
+    return(
+        <div>
+            <h1>Welcome To online Restaurant</h1>
+            <h2>OUR MENU</h2>
+        </div>
+      
+
+    )
+}
+
+export default Landing
